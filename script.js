d4877cb5a8d87be4343e85f09d264e13edf42b4b
Gabriel Belloni <gabriel.belloni@outlook.com>
2020-02-11 12:56:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$('#valorVenda').maskMoney();
+
+$(".inputNumber").keyup(function(){
+	var valorVenda = document.getElementById("valorVenda").value;
+	var numeroParcelas = document.getElementById("numeroParcelas").value;
+	var percentualMdr = document.getElementById("percentualMdr").value;
+
+	if (valorVenda != "" && numeroParcelas != "" && percentualMdr != "") {
+	}
+		
+});
