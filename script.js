165bdd27a88e38cb6fce982f70c7c971f824f1b7
Gabriel Belloni <gabriel.belloni@outlook.com>
2020-03-11 14:41:40 -0300

    update index and script

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 $('#valorVenda').maskMoney();
 
-$(".inputNumber").keyup(function(){
-	var valorVenda = document.getElementById("valorVenda").value;
-	var numeroParcelas = document.getElementById("numeroParcelas").value;
-	var percentualMdr = document.getElementById("percentualMdr").value;
+function validarCampos(){
+	getValores();
 
 	if (valorVenda != "" && numeroParcelas != "" && percentualMdr != "") {
-		alert(numeroParcelas);
 		/*if (numeroParcelas > 12){
 			$("#numeroParcelas").addClass("Warning");
 		}*/
+		console.log(valorVenda.value.lenght);
+		console.log(numeroParcelas.value.lenght);
+		console.log(percentualMdr.value.lenght);
+
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify({"amount":valorVenda,"installments":numeroParcelas,"mdr":percentualMdr});
+
+		var requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+		};
+
+		fetch("https://hash-front-test.herokuapp.com/", requestOptions)
+		.then(response => response.text())
+		.then(result => console.log(result))
+		.catch(error => console.log('error', error));
 	}
-		
-});
+
+}
+
+function getValores(){
+	var valorVenda = document.getElementById("valorVenda").value;
+	var numeroParcelas = document.getElementById("numeroParcelas").value;
+	var percentualMdr = document.getElementById("percentualMdr").value;
+}
